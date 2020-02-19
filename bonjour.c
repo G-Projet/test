f22d845343bbc755f795afd3ef1fe7c6b2d095cc
cpiau <claudine.piau@gmail.com>
2020-02-19 15:59:33 +0100

    commit 1

---
+++ bonjour.c
@@ -0,0 +1,15 @@
+//
+//  bonjour.c
+//  
+//
+//  Created by Claudine Piau-Toffolon on 09/01/2018.
+//
+//
+#include<stdio.h>
+#include <stdlib.h>
+void Bonjour (void)
+{
+    /* afficher le mot bonjour et saut de ligne */
+    printf("Bonjour!!\n");
+    
+}
