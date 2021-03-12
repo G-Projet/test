f3821ad36a73ef5ed1f8142b39c379a0e44a43c9
Smail <Belkadi@Smail.com>
2021-03-12 11:04:24 +0100

    ajout

---
+++ fonction.h
@@ -0,0 +1,9 @@
+
+
+extern void bienvenus();
+
+extern void evenement (SDL_bool program_launched );
+
+extern char message_erreurs(const char*);
+
+extern void clean_ressources(SDL_Window*w, SDL_Renderer *r, SDL_Texture *t);
